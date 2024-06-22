8d0701b1e4c80cce1f11c74fa28998d8a6fa621d
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-22 19:24:15 -0300

    feat: refactor mongoose connecttion string

--- backend/src/lib/mongoose.ts
+++ backend/src/lib/mongoose.ts
@@ -1,4 +1,7 @@
 import Mongoose from "mongoose";
+import { ENV } from "../static/envs";
+
+const env = process.env.NODE_ENV || ENV.DEVELOPMENT;
 
 export class MongooseServer {
   static connection: Mongoose.Connection | undefined;
@@ -7,10 +10,10 @@ export class MongooseServer {
     if (this.connection) {
       await this.disconnectServer();
     }
-
-    Mongoose.connect(process.env.MONGO_DB_CONNECTION_STRING!)
+    const connectionString = this.getConnectionString();
+    await Mongoose.connect(connectionString)
       .then(() => {
-        console.log("Connected to MongoDB");
+        console.log("Connected to MongoDB, env: ", env);
         this.connection = Mongoose.connection;
       })
       .catch((err) => {
@@ -21,5 +24,16 @@ export class MongooseServer {
   static disconnectServer = async () => {
     await Mongoose.disconnect();
     this.connection = undefined;
+  };
+
+  static getConnectionString = () => {
+    let connectionString = process.env.MONGO_DB_CONNECTION_STRING!;
+    if (!connectionString?.endsWith("/")) {
+      connectionString = connectionString + "/";
+    }
+    connectionString = connectionString + `${env}`;
+    const complement = process.env.MONGO_DB_CONNECTION_STRING_COMPLEMENT || "";
+    connectionString = connectionString + complement;
+    return connectionString;
   };
 }
