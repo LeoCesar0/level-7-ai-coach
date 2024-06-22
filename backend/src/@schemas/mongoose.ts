f69da653737f0c582e2a045a920a91a952b8cdff
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-22 19:23:39 -0300

    feat: update mongoose schema

--- backend/src/@schemas/mongoose.ts
+++ backend/src/@schemas/mongoose.ts
@@ -1,4 +1,12 @@
 import { zId } from "@zodyac/zod-mongoose";
 import { z } from "zod";
 
-export const zMongooseBase = z.object({ _id: zId, __v: z.number().optional() });
+export const zMongoDocument = z.object({
+  _id: zId,
+  __v: z.number().optional(),
+});
+
+export type MongoDocument = {
+  _id: string;
+  __v: number;
+};
