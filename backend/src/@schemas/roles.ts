6c76ca2a2c1570528aee87b14a78cc45969856d8
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-28 18:58:59 -0300

    feat: add chat static roles

--- backend/src/@schemas/roles.ts
+++ backend/src/@schemas/roles.ts
@@ -3,4 +3,8 @@ import { ROLES_LIST } from "../static/roles";
 
 export const zRole = z.enum(["admin", "user", "coach"]);
 
+export const zChatRole = z.enum(["user", "system", "assistant"]);
+
 export type Role = z.infer<typeof zRole>;
+
+export type ChatRole = z.infer<typeof zChatRole>;
--- backend/src/static/roles.ts
+++ backend/src/static/roles.ts
@@ -1,9 +1,19 @@
-import { Role } from "../@schemas/roles";
+import { ChatRole, Role } from "../@schemas/roles";
 
 export const ROLE = {
   ADMIN: "admin",
   USER: "user",
   COACH: "coach",
-};
+} as const
+
+export const CHAT_ROLE = {
+  USER: "user",
+  SYSTEM: "system",
+  ASSISTANT: "assistant",
+} as const;
 
 export const ROLES_LIST: Role[] = Object.values(ROLE) as Role[];
+
+export const CHAT_ROLES_LIST: ChatRole[] = Object.values(
+  CHAT_ROLE
+) as ChatRole[];
