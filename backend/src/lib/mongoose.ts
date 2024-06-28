9503ffbfdf7a7060e70ba7f375f9f8125dcc3d44
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-28 19:01:02 -0300

    feat: ensure load envs and better logs in mongoose lib

--- backend/src/lib/mongoose.ts
+++ backend/src/lib/mongoose.ts
@@ -1,5 +1,8 @@
 import Mongoose from "mongoose";
 import { ENV } from "../static/envs";
+import dotenv from "dotenv";
+
+dotenv.config({ path: "../.env" });
 
 const env = process.env.NODE_ENV || ENV.DEVELOPMENT;
 
@@ -8,12 +11,15 @@ export class MongooseServer {
 
   static connectServer = async () => {
     if (this.connection) {
+      console.log(
+        "Already connected to MongoDB, disconnecting previous server"
+      );
       await this.disconnectServer();
     }
     const connectionString = this.getConnectionString();
     await Mongoose.connect(connectionString)
       .then(() => {
-        console.log("Connected to MongoDB, env: ", env);
+        console.log(`🖥️ 🌏 Connected to MongoDB env: ${env}`);
         this.connection = Mongoose.connection;
       })
       .catch((err) => {
@@ -22,12 +28,16 @@ export class MongooseServer {
   };
 
   static disconnectServer = async () => {
+    console.log("🔌 MongoDB Server disconnecting");
     await Mongoose.disconnect();
     this.connection = undefined;
   };
 
   static getConnectionString = () => {
     let connectionString = process.env.MONGO_DB_CONNECTION_STRING!;
+    if (!connectionString) {
+      throw new Error("Mongo db connection string is not set");
+    }
     if (!connectionString?.endsWith("/")) {
       connectionString = connectionString + "/";
     }
