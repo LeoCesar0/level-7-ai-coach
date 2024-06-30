d3196d1a77b266f9aa155d7dd42cdcbfd4293c76
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-06-30 16:10:01 -0300

    refactor: users test suite

--- backend/tests/integration/users.test.ts
+++ backend/tests/integration/users.test.ts
@@ -43,167 +43,178 @@ describe("users and organizations integration suite", () => {
     }
   });
 
-  // --------------------------
-  // CREATE NEW USER
-  // --------------------------
-  it("should create a new user, by admin", async () => {
-    if (!_organization) {
-      throw new Error("Organization was not created");
-    }
+  describe("create", () => {
     // --------------------------
-    // ARRANGE
+    // CREATE NEW USER
     // --------------------------
+    it("should create a new user, by admin", async () => {
+      if (!_organization) {
+        throw new Error("Organization was not created");
+      }
+      // --------------------------
+      // ARRANGE
+      // --------------------------
 
-    stub = await stubGetUserFromToken(_seed.admin);
+      stub = await stubGetUserFromToken(_seed.admin);
 
-    const userOnCreate: ICreateUser = {
-      active: true,
-      name: _userName,
-      role: "user",
-      organization: _organization._id,
-      email: _userEmail,
-    };
-    const body: ISignUpRoute = {
-      password: "123456789",
-      user: userOnCreate,
-    };
+      const userOnCreate: ICreateUser = {
+        active: true,
+        name: _userName,
+        role: "user",
+        organization: _organization._id,
+        email: _userEmail,
+      };
+      const body: ISignUpRoute = {
+        password: "123456789",
+        user: userOnCreate,
+      };
 
-    // --------------------------
-    // ACT
-    // --------------------------
+      // --------------------------
+      // ACT
+      // --------------------------
 
-    const res = await honoApp.request("/api/users", {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer 123",
-      },
+      const res = await honoApp.request("/api/users", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 123",
+        },
+      });
+
+      const json: AppResponse<IUser> = await res.json();
+
+      const user = json.data;
+
+      if (json.error) {
+        console.log("❌ Create error", json.error);
+      }
+
+      // --------------------------
+      // ASSERT
+      // --------------------------
+
+      expect(res.status).toBe(200);
+      expect(json.error).toBe(null);
+      expect(user?.name).toBe(userOnCreate.name);
+      expect(user?._id).toBeTruthy();
+      expect(user?.firebaseId).toBeTruthy();
+
+      _createdUser = user;
     });
-
-    const json: AppResponse<IUser> = await res.json();
-
-    const user = json.data;
-
-    if (json.error) {
-      console.log("❌ Create error", json.error);
-    }
-
     // --------------------------
-    // ASSERT
+    // VERIFY SET ORGANIZATION
     // --------------------------
+    it("should set created user to the created organization", async () => {
+      if (!_createdUser) {
+        throw new Error("User not created");
+      }
+      if (!_organization) {
+        throw new Error("Organization not created");
+      }
+      const updatedOrgRes = await OrganizationModel.findById(
+        _organization?._id
+      );
 
-    expect(res.status).toBe(200);
-    expect(json.error).toBe(null);
-    expect(user?.name).toBe(userOnCreate.name);
-    expect(user?._id).toBeTruthy();
-    expect(user?.firebaseId).toBeTruthy();
+      const updatedOrg = updatedOrgRes?.toObject();
 
-    _createdUser = user;
-  });
-  // --------------------------
-  // LIST USER
-  // --------------------------
-  it("should list and find created user", async () => {
-    stub = await stubGetUserFromToken(_seed.admin);
-
-    const res = await honoApp.request("/api/users/list", {
-      method: "POST",
-      body: JSON.stringify({}),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer 123",
-      },
+      const found = updatedOrg?.users.find(
+        (id) => id.toString() === _createdUser?._id
+      );
+      expect(found).toBeTruthy();
     });
+    // --------------------------
+    // DENY CREATE SAME USER
+    // --------------------------
+    it("should not create user with same email, user exists", async () => {
+      if (!_createdUser) {
+        throw new Error("User not created");
+      }
 
-    const json: AppResponse<IPaginationResult<IUser>> = await res.json();
+      stub = await stubGetUserFromToken(_seed.admin);
 
-    const found = json.data?.list.find(
-      (item) => item._id === _createdUser?._id
-    );
+      const userOnCreate: ICreateUser = {
+        active: true,
+        name: _userName,
+        role: "user",
+        organization: _organization!._id,
+        email: _createdUser.email,
+      };
+      const body: ISignUpRoute = {
+        password: "123456789",
+        user: userOnCreate,
+      };
 
-    expect(res.status).toBe(200);
-    expect(found).toBeTruthy();
-  });
-  // --------------------------
-  // GET USER
-  // --------------------------
-  it("should get the created user", async () => {
-    if (!_createdUser) {
-      throw new Error("User not created");
-    }
+      const res = await honoApp.request("/api/users", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer any-token",
+        },
+      });
 
-    const res = await honoApp.request(`/api/users/${_createdUser._id}`);
+      const json: AppResponse<IUser> = await res.json();
 
-    const json: AppResponse<IUser> = await res.json();
-
-    const user = json.data;
-
-    expect(res.status).toBe(200);
-    expect(user?.name).toBe(_createdUser.name);
-    expect(user?.firebaseId).toBe(_createdUser.firebaseId);
-    expect(user?._id).toBe(_createdUser._id);
-    expect(user?.organization).toBeTruthy();
-  });
-  // --------------------------
-  // VERIFY SET ORGANIZATION
-  // --------------------------
-  it("should set created user to the created organization", async () => {
-    if (!_createdUser) {
-      throw new Error("User not created");
-    }
-    if (!_organization) {
-      throw new Error("Organization not created");
-    }
-    const updatedOrgRes = await OrganizationModel.findById(_organization?._id);
-
-    const updatedOrg = updatedOrgRes?.toObject();
-
-    const found = updatedOrg?.users.find(
-      (id) => id.toString() === _createdUser?._id
-    );
-    expect(found).toBeTruthy();
-  });
-  // --------------------------
-  // DENY CREATE SAME USER
-  // --------------------------
-  it("should not create user with same email, user exists", async () => {
-    if (!_createdUser) {
-      throw new Error("User not created");
-    }
-
-    stub = await stubGetUserFromToken(_seed.admin);
-
-    const userOnCreate: ICreateUser = {
-      active: true,
-      name: _userName,
-      role: "user",
-      organization: _organization!._id,
-      email: _createdUser.email,
-    };
-    const body: ISignUpRoute = {
-      password: "123456789",
-      user: userOnCreate,
-    };
-
-    const res = await honoApp.request("/api/users", {
-      method: "POST",
-      body: JSON.stringify(body),
-      headers: {
-        "Content-Type": "application/json",
-        Authorization: "Bearer any-token",
-      },
+      expect(res.status).toBe(400);
+      expect(json.error?.message).toBe(EXCEPTIONS.USER_ALREADY_EXISTS);
     });
-
-    const json: AppResponse<IUser> = await res.json();
-
-    expect(res.status).toBe(400);
-    expect(json.error?.message).toBe(EXCEPTIONS.USER_ALREADY_EXISTS);
   });
-  // --------------------------
-  // UPDATE USER
-  // --------------------------
+
+  describe("list", () => {
+    // --------------------------
+    // LIST USER
+    // --------------------------
+    it("should list and find created user", async () => {
+      stub = await stubGetUserFromToken(_seed.admin);
+
+      const res = await honoApp.request("/api/users/list", {
+        method: "POST",
+        body: JSON.stringify({}),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 123",
+        },
+      });
+
+      const json: AppResponse<IPaginationResult<IUser>> = await res.json();
+
+      const found = json.data?.list.find(
+        (item) => item._id === _createdUser?._id
+      );
+
+      expect(res.status).toBe(200);
+      expect(found).toBeTruthy();
+    });
+  });
+
+  describe("get", () => {
+    // --------------------------
+    // GET USER
+    // --------------------------
+    it("should get the created user", async () => {
+      if (!_createdUser) {
+        throw new Error("User not created");
+      }
+
+      const res = await honoApp.request(`/api/users/${_createdUser._id}`);
+
+      const json: AppResponse<IUser> = await res.json();
+
+      const user = json.data;
+
+      expect(res.status).toBe(200);
+      expect(user?.name).toBe(_createdUser.name);
+      expect(user?.firebaseId).toBe(_createdUser.firebaseId);
+      expect(user?._id).toBe(_createdUser._id);
+      expect(user?.organization).toBeTruthy();
+    });
+  });
+
   describe("update", () => {
+    // --------------------------
+    // UPDATE USER
+    // --------------------------
     let _updatedUser: IUser | null = null;
     it("should not update user info, not the same user", async () => {
       if (!_createdUser) {
