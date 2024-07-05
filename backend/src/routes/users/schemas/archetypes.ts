3314fff84c908971942de79bffd174e98d39f0db
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-07-05 19:16:20 -0300

    feat: add archetype schema

--- backend/src/routes/users/schemas/archetypes.ts
+++ backend/src/routes/users/schemas/archetypes.ts
@@ -1,8 +1,3 @@
-
-export const ARCHETYPES = {
-    theRisingStart: 'The Rising Star: Young athletes with high potential benefit significantly from targeted development and motivational strategies. Focusing on their growth and skill enhancement can help them reach their peak performance early and sustain it over time.'
-}
-
 // The Rising Star: Young athletes with high potential benefit significantly from targeted development and motivational strategies. Focusing on their growth and skill enhancement can help them reach their peak performance early and sustain it over time.
 // The Consistent Performer: Athletes who maintain a steady level of high performance require strategies to prevent burnout and maintain motivation. Ensuring they have the support to fine-tune their skills can help them stay at the top of their game for longer periods.
 // The Comeback Athlete: Rehabilitation and mental resilience are crucial for athletes recovering from injuries or poor performance. Tailored rehabilitation programs and mental toughness training can aid in their gradual return to peak condition​(PLOS)​​ (ERIC)​.
