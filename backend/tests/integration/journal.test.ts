82ce2e548c99866a0b5c9448cd7d87e2302ceb1e
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-07-07 22:27:59 -0300

    feat: add journal tests

---
+++ backend/tests/integration/journal.test.ts
@@ -0,0 +1,176 @@
+import honoApp from "../../src";
+import { AppResponse } from "../../src/@schemas/app";
+import { IPaginationResult } from "../../src/@schemas/pagination";
+import {
+  JournalModel,
+  IJournal,
+} from "../../src/routes/journal/schemas/journal";
+import { EXCEPTIONS } from "../../src/static/exceptions";
+import { stubGetUserFromToken } from "../helpers/stubGetUserFromToken";
+import { ISeedResult, TestServer } from "../mongodb-memory-server";
+import sinon from "sinon";
+import { ICreateJournal } from "../../src/routes/journal/schemas/createJournal";
+
+describe("journal integration suite", () => {
+  console.log("🔻 Enter JOURNAL integration suite  -->");
+
+  let _createdJournal: IJournal | null = null;
+  let _journalText = "My first journal entry";
+  let _journalUpdatedText = "Updated journal entry";
+
+  let _seed: ISeedResult;
+  let stub: sinon.SinonStub<any>;
+
+  beforeAll(async () => {
+    await TestServer.connectTestServer();
+    _seed = (await TestServer.seedTestServer())!;
+  });
+
+  afterAll(async () => {
+    await TestServer.disconnectTestServer();
+  });
+
+  afterEach(() => {
+    if (stub) {
+      stub.restore();
+    }
+  });
+
+  describe("create", () => {
+    it("should create a new journal entry, by user", async () => {
+      stub = await stubGetUserFromToken(_seed.normalUser);
+
+      const body: ICreateJournal = {
+        date: new Date().toISOString(),
+        text: _journalText,
+        draft: false,
+        images: [],
+      };
+
+      const res = await honoApp.request("/api/journal", {
+        method: "POST",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 123",
+        },
+      });
+
+      const json: AppResponse<IJournal> = await res.json();
+      const journal = json.data;
+
+      if (json.error) {
+        console.log("❌ Create journal error", json.error);
+      }
+
+      expect(res.status).toBe(200);
+      expect(json.error).toBe(null);
+      expect(journal?.text).toBe(_journalText);
+
+      _createdJournal = journal;
+    });
+  });
+
+  describe("list", () => {
+    it("should list and find created journal entry", async () => {
+      stub = await stubGetUserFromToken(_seed.normalUser);
+
+      const res = await honoApp.request("/api/journal/list", {
+        method: "POST",
+        body: JSON.stringify({}),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer 123",
+        },
+      });
+
+      const json: AppResponse<IPaginationResult<IJournal>> = await res.json();
+      const found = json.data?.list.find(
+        (item) => item._id === _createdJournal?._id
+      );
+
+      expect(res.status).toBe(200);
+      expect(found).toBeTruthy();
+      expect(found?.text).toBe(_journalText);
+    });
+  });
+
+  describe("get", () => {
+    it("should get the created journal entry", async () => {
+      if (!_createdJournal) {
+        throw new Error("Journal not created");
+      }
+      stub = await stubGetUserFromToken(_seed.normalUser);
+
+      const res = await honoApp.request(`/api/journal/${_createdJournal._id}`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer any-token",
+        },
+      });
+
+      const json: AppResponse<IJournal> = await res.json();
+      const journal = json.data;
+
+      if (json.error) {
+        console.log("❗ Get journal error -->", json.error);
+      }
+
+      expect(res.status).toBe(200);
+      expect(journal?.text).toBe(_journalText);
+    });
+  });
+
+  describe("update", () => {
+    it("should update the journal entry", async () => {
+      if (!_createdJournal) {
+        throw new Error("Journal not created");
+      }
+      stub = await stubGetUserFromToken(_seed.normalUser);
+
+      const body: ICreateJournal = {
+        text: _journalUpdatedText,
+        draft: false,
+        images: [],
+      };
+
+      const res = await honoApp.request(`/api/journal/${_createdJournal._id}`, {
+        method: "PUT",
+        body: JSON.stringify(body),
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer any-token",
+        },
+      });
+
+      const json: AppResponse<IJournal> = await res.json();
+      const journal = json.data;
+
+      expect(res.status).toBe(200);
+      expect(journal?.text).toBe(_journalUpdatedText);
+    });
+  });
+
+  describe("delete", () => {
+    it("should delete the journal entry", async () => {
+      if (!_createdJournal) {
+        throw new Error("Journal not created");
+      }
+      stub = await stubGetUserFromToken(_seed.normalUser);
+
+      const res = await honoApp.request(`/api/journal/${_createdJournal._id}`, {
+        method: "DELETE",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer any-token",
+        },
+      });
+
+      const json: AppResponse<boolean> = await res.json();
+
+      expect(res.status).toBe(200);
+      expect(json.data).toBe(true);
+    });
+  });
+});
