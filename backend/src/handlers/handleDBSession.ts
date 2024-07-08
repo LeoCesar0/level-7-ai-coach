a287da5210289ef513a264bdf7a7dff7019dfc1b
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-07-07 22:27:48 -0300

    fix: test db session

--- backend/src/handlers/handleDBSession.ts
+++ backend/src/handlers/handleDBSession.ts
@@ -6,15 +6,15 @@ export const handleDBSession = async <T>(
   const session = await mongoose.startSession();
   try {
     session.startTransaction();
-    console.log("❗❗❗ Here 1");
+    // console.log("❗❗❗ Here 1");
     const result = await fn(session);
-    console.log("❗❗❗ Here 2");
+    // console.log("❗❗❗ Here 2");
     await session.commitTransaction();
     await session.endSession();
-    console.log("❗❗❗ Here 3");
+    // console.log("❗❗❗ Here 3");
     return result;
   } catch (err) {
-    console.log("❗ handleDBSession ERR -->", err);
+    // console.log("❗ handleDBSession ERR -->", err);
     await session.abortTransaction();
     await session.endSession();
     throw err;
--- backend/tests/unit/handleDBSession.test.ts
+++ backend/tests/unit/handleDBSession.test.ts
@@ -1,11 +1,11 @@
 import { handleDBSession } from "../../src/handlers/handleDBSession";
+import { slugify } from "../../src/helpers/slugify";
 import { MongooseServer } from "../../src/lib/mongoose";
 import { ArchetypeModel } from "../../src/routes/archetype/schemas/archetype";
+import { ENV } from "../../src/static/envs";
 import { ISeedResult, TestServer } from "../mongodb-memory-server";
 
 describe("handleDBSession unit suite", () => {
-  let _seed: ISeedResult;
-
   let itemName = "handleDBSession TEST";
   let itemName1 = itemName + " 1";
   let itemName2 = itemName + " 2";
@@ -13,12 +13,20 @@ describe("handleDBSession unit suite", () => {
   let slug = "slug-test-handle-db-session";
 
   beforeAll(async () => {
-    await TestServer.connectTestServer();
-    _seed = (await TestServer.seedTestServer())!;
+    // await TestServer.connectTestServer();
+    if (process.env.NODE_ENV !== ENV.TEST) {
+      throw new Error("NODE_ENV is not TEST");
+    }
+    await MongooseServer.connectServer();
   });
 
   afterAll(async () => {
-    await TestServer.disconnectTestServer();
+    // await TestServer.disconnectTestServer();
+    await MongooseServer.disconnectServer();
+  });
+
+  beforeEach(async () => {
+    await ArchetypeModel.deleteMany();
   });
 
   it("should ensure item is created when expected", async () => {
@@ -41,10 +49,41 @@ describe("handleDBSession unit suite", () => {
     createdItem = await ArchetypeModel.findOne({ name: itemName });
 
     expect(createdItem).toBeTruthy();
-
-    await ArchetypeModel.deleteOne({ _id: createdItem?._id });
   });
-  it("should handle db session", async () => {
+  it("should insert", async () => {
+    try {
+      await handleDBSession(async (session) => {
+        await ArchetypeModel.create(
+          [
+            {
+              name: itemName1,
+              slug: slug,
+              description: "desc",
+            },
+          ],
+          { session }
+        );
+
+        await ArchetypeModel.create(
+          [
+            {
+              name: itemName2,
+              slug: slugify(itemName2),
+              description: "desc",
+            },
+          ],
+          { session }
+        );
+      });
+    } catch (err) {}
+
+    const createdItem1 = await ArchetypeModel.findOne({ name: itemName1 });
+    const createdItem2 = await ArchetypeModel.findOne({ name: itemName2 });
+
+    expect(createdItem1).toBeTruthy();
+    expect(createdItem2).toBeTruthy();
+  });
+  it("should not insert, one of two are invalid", async () => {
     try {
       await handleDBSession(async (session) => {
         await ArchetypeModel.create(
