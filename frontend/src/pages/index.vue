389fc8c03496418028e2d66f156824a6d9073c3e
leonardo.rocha <leonardo.rocha@grupofolha.com.br>
2024-07-24 17:40:50 -0300

    feat: update logo color

--- frontend/src/pages/index.vue
+++ frontend/src/pages/index.vue
@@ -9,7 +9,7 @@ let num: number = 2;
     <div class="mb-16 relative">
       <h1 class="animate-fade-up text-5xl mb-6">Welcome to</h1>
       <div class="animate-fade-up delay-75 flex items-center pl-6 gap-1">
-        <Logo :size="40" />
+        <Logo :size="40" class="text-primary" />
         <h1 class="text-5xl">Level 7 AI</h1>
       </div>
       <div class="absolute bottom-[calc(-100%-64px)]">
